7700abde69cb3b24ba32ce440052aecc8b9e5e3c
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-19 13:25:55 +0530

    Install redux and redux toolkit
    Creat a folder named api with js file which should use axios tool for better JSON response of an api,
    Create reducers folder under store folder with store.js file,
    Add provider and mention store.js file as global data accessable provider.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import store from './store/reducers/store.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
 )
