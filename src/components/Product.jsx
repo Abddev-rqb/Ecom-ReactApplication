b88d18c702166d5fb4b3b2ef2a9495bc4aa53f82
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-20 13:30:03 +0530

    Fetch products from API
    Create api folder and set the backend url,
    Access the url from the api and fetch the products via url to dispatch,
    Use the fetch type of the api in the reducer,
    Add the reducer in the store file of the reducer folder,
    Declare the provider with store in the main.jsx file which provides reducer,
    Dispatch the api call which fetchs the product data in the product.jsx file.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,29 +1,17 @@
 import { FaExclamationTriangle } from 'react-icons/fa'
 import ProductCard from './ProductCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchProducts } from '../store/actions';
 
 const Product = ()=>{
     const isLoading = false;
     const errorMessage ="";
-    const products= [{
-            productId: 652,
-            productName: "IPhone Xs max",
-            image:"https://placehold.co/600x400",
-            description:"This model is on demand to this new market",
-            quantity:0,
-            price:1450.0,
-            discount:10.0,
-            specialPrice:1305.0,
-        },{
-            productId: 654,
-            productName:"MacBook Air m25",
-            image:"https://placehold.co/600x400",
-            description:"Air book is binded with enlarged cloud storage",
-            quantity:50,
-            price:2550.0,
-            discount:20.0,
-            specialPrice:2040.0,
-        }
-    ];
+    const {products} = useSelector((state) => state.products);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(fetchProducts());
+    },[dispatch]);
     return(
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             {isLoading ?(
