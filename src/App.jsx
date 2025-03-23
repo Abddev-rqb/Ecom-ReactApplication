f9ba9c78d97b6ac089838b8f452054d3cb684e7b
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-23 06:30:16 +0530

    Create Navigation bar
    Create a navBar and make it available to the site with primary components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Product from './components/products/Product'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
+import Navbar from './components/shared/Navbar'
 
 function App() {
   return (
     <>
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/products" element={<Product />}/>
