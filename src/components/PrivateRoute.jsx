6283c1b679305478550a5904f5588c56e680c95e
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-25 11:50:38 +0530

    Create a redirection for the logged in user
    Create private route to redirect logged in user from reaching login in page to profile page,
    Create a route tag and pass the profile route tag to perform redirection.

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = ({publicPage = false}) =>{
+    const { user } = useSelector((state) => state.auth);
+    if (publicPage){
+        return user ? <Navigate to="/profile" /> : <Outlet/>
+    }
+    return user ? <Outlet/> : <Navigate to="/profile" /> 
+};
+export default PrivateRoute;
--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -4,7 +4,7 @@ import { errorReducer } from "./errorReducer";
 import { cartReducer } from "./cartReducer";
 import { authReducer } from "./authReducer";
 
-const user = localStorage.getItem("auth") ? JSON.parse(localStorage.getItem("auth")):[];
+const user = localStorage.getItem("auth") ? JSON.parse(localStorage.getItem("auth")):null;
 const cartItems = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")):[];
 
 const initialState = { auth:{user: user}, carts :{cart:cartItems},};
